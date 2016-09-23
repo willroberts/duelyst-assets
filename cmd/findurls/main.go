c716e60ad8a4fe10d7eb45ab8aa0e2d6e84206ae
Will Roberts <will.roberts11@gmail.com>
2016-09-22 20:33:23 -0700

    Updates findurls

--- cmd/findurls/main.go
+++ cmd/findurls/main.go
@@ -1,3 +1,5 @@
+// findurls prints the unique URLs found in Duelyst's source code.
+// This program expects to be executed from the root of this repo.
 package main
 
 import (
@@ -8,13 +10,17 @@ import (
 )
 
 func main() {
-	b, err := ioutil.ReadFile("src/duelyst.js")
+	bytes, err := ioutil.ReadFile("src/duelyst.js")
 	if err != nil {
 		panic(err)
 	}
-	s := string(b)
-	urls := xurls.Strict.FindAllString(s, -1)
-	for _, u := range urls {
-		fmt.Println(u)
+	urls := xurls.Strict.FindAllString(string(bytes), -1)
+	uniqueURLs := make(map[string]bool)
+	for _, url := range urls {
+		_, ok := uniqueURLs[url]
+		if !ok {
+			fmt.Println(url)
+			uniqueURLs[url] = true
+		}
 	}
 }
